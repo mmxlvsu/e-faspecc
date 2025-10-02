e081757068729364dac92a854cbd95228e79037f
mmxlvsu <laplap.mariel05@gmail.com>
2025-10-02 11:42:13 +0800

    new

--- my-mobile-app/src/pages/Splash.jsx
+++ my-mobile-app/src/pages/Splash.jsx
@@ -35,41 +35,41 @@ export default function Splash() {
         }}
       />
 
-      {/* Buttons container */}
-      <div className="absolute bottom-[15%] left-1/2 -translate-x-1/2 w-[85%] flex flex-col gap-4 z-30">
+      {/* Buttons */}
+      <div className="absolute z-30 left-[6%] top-[52%] w-[88%]">
         <button
           onClick={handleGetStarted}
-          className="w-full py-4 bg-black text-white text-lg font-bold rounded-lg"
+          className="w-full h-[8vh] bg-black text-white text-[4.5vw] font-bold rounded-lg"
         >
           Get Started
         </button>
-
-        <div className="flex items-center gap-2">
-          <button
-            onClick={handleGoogle}
-            className="w-full py-4 bg-white text-black text-lg font-bold rounded-lg flex items-center justify-center gap-2"
-          >
-            <img src={logo1} alt="Google" className="w-5 h-5" />
-            Continue with Google
-          </button>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <button
-            onClick={handleFacebook}
-            className="w-full py-4 bg-white text-black text-lg font-bold flex items-center justify-center gap-2 rounded-lg"
-          >
-            <img src={logo2} alt="Facebook" className="w-5 h-5" />
-            Continue with Facebook
-          </button>
-        </div>
       </div>
 
-      {/* Social sign up text */}
-      <div className="absolute bottom-[27%] left-1/2 -translate-x-1/2 w-3/4 flex items-center z-30">
+      <div className="absolute z-30 left-[6%] top-[64%] w-[88%]">
+        <button
+          onClick={handleGoogle}
+          className="w-full h-[8vh] bg-white text-black text-[4.5vw] font-bold rounded-lg flex items-center justify-center gap-2"
+        >
+          <img src={logo1} alt="Google" className="w-5 h-5" />
+          Continue with Google
+        </button>
+      </div>
+
+      <div className="absolute z-30 left-[6%] top-[74%] w-[88%]">
+        <button
+          onClick={handleFacebook}
+          className="w-full h-[8vh] bg-white text-black text-[4.5vw] font-bold flex items-center justify-center gap-2 rounded-lg"
+        >
+          <img src={logo2} alt="Facebook" className="w-5 h-5" />
+          Continue with Facebook
+        </button>
+      </div>
+
+      {/* Extra texts - centered with lines */}
+      <div className="absolute z-30 left-1/2 top-[60.5%] w-3/4 -translate-x-1/2 flex items-center">
         <div className="flex-grow border-t border-white"></div>
         <span
-          className="mx-2 text-sm text-white text-center cursor-pointer"
+          className="mx-2 text-[3.5vw] text-white text-center cursor-pointer"
           onClick={() => alert("Social sign up clicked!")}
         >
           or use social sign up
@@ -78,7 +78,7 @@ export default function Splash() {
       </div>
 
       {/* Log in text */}
-      <p className="absolute bottom-[6%] left-1/2 -translate-x-1/2 w-3/4 text-sm text-white text-center z-30">
+      <p className="absolute z-30 left-1/2 top-[85%] w-3/4 -translate-x-1/2 text-[3.5vw] text-white text-center">
         Already have an account?{" "}
         <span
           onClick={handleLogin}
