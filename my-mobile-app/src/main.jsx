a4c3701637d43a0c558ec646b0aae2ffbc48222f
mmxlvsu <laplap.mariel05@gmail.com>
2025-10-03 11:43:52 +0800

    verify testing 2

--- my-mobile-app/src/main.jsx
+++ my-mobile-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import Home from "./pages/Reset";   
+import Home from "./pages/Verify";   
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
