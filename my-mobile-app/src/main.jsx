70eeba7876e039912353f92d3467024931feef0c
mmxlvsu <laplap.mariel05@gmail.com>
2025-10-04 20:33:36 +0800

    last na

--- my-mobile-app/src/main.jsx
+++ my-mobile-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import Home from "./pages/Cart";   
+import Home from "./pages/Profile";   
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
